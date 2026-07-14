c39fe9d7cd4c7e9bcb04b7811db3e3512c28e60e
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-07-14 22:09:13 +0600

    chore: add node polyfills to fix vercel white screen

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   define: { global: "globalThis" },
   plugins: [
     react(),
+    nodePolyfills(),
   ],
   build: {
     rollupOptions: {
       external: [
         /^__vite-optional-peer-dep:/,
         '@solana-program/system',
-        '@solana/kit'
+        '@solana/kit',
+        '@solana-program/token',
+        '@solana-program/token-2022',
+        '@solana/sysvars'
       ]
     }
   }
